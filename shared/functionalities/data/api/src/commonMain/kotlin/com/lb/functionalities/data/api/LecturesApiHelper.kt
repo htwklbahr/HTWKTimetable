a771cd849e79ebf453d6e35f23a141fb15533688
Luis Bahr <luis.bahr@appsfactory.de>
2023-12-30 21:55:13 +0100

    [UPD] common cleanup and comments.

--- shared/functionalities/data/api/src/commonMain/kotlin/com/lb/functionalities/data/api/LecturesApiHelper.kt
+++ shared/functionalities/data/api/src/commonMain/kotlin/com/lb/functionalities/data/api/LecturesApiHelper.kt
@@ -3,16 +3,18 @@ package com.lb.functionalities.data.api
 import com.lb.functionalities.data.api.model.*
 import com.lb.functionalities.data.entities.LectureObjectDto
 
+/**
+ * Helper class setting the functions for receiving the data.
+ */
 class LecturesApiHelper {
 
     fun getAllLectures(): List<LectureObjectDto> =
         LecturesApiService().getDummyLectureData().map { lectureObject ->
             lectureObject.toDto()
         }
-
-
 }
 
+// Different approach for accessing the API. Currently not used.
 interface ApiHelper {
     companion object {
         fun getApiAccess(): ApiHelper {
